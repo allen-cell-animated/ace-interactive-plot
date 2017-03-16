1beb6be3e0bd6db68e257d40e98402e107223d74
chughes <chrishu@alleninstitute.org>
2017-03-16 16:27:42 -0700

    cell-data.json file

--- js/AICSChart.js
+++ js/AICSChart.js
@@ -38,27 +38,8 @@ function AICSChart(model, my){
             return;
         }
         queue()
-            .defer(d3.csv, model.dataFile)
-            .defer(d3.csv, model.imageDataFile)
-            .await(function(error, data, imageData) {
-                var imageIdFileMap = imageData.reduce(function ( total, current ) {
-                    total[ current.filename.split('.czi')[0] ] = current.cellline;
-                    return total;
-                }, {});
-
-                data.forEach(function (element) {
-                    var im_ids_split = element.im_ids.split('_');
-                    // var cellline = imageIdFileMap[im_ids_split.slice(0, im_ids_split.length-1).join('_')] + '_' + im_ids_split[im_ids_split.length-1];
-                    // if('undefined' == cellline.split('_')[0]){
-                    //     console.log(im_ids_split.slice(0, im_ids_split.length-1).join('_'));
-                    //     return;
-                    // }
-                    element.cellName = imageIdFileMap[im_ids_split.slice(0, im_ids_split.length-1).join('_')] + '_' + im_ids_split[im_ids_split.length-1];
-                    // element.imageFilePath = model.imagesDir + '/' + element.cellName.split('_')[0] + '/' + element.cellName + '.png';
-                });
-                // data = data.filter(function (d) {
-                //     return d.imageFilePath;
-                // });
+            .defer(d3.json, model.dataFile)
+            .await(function(error, data) {
                 var chart = d3.select('#' + model.parent)
                     .append('svg')
                     .attr('width', model.chartWidth + model.margin.right + model.margin.left)
